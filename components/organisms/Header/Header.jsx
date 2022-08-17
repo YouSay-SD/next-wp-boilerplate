51880e71d94efbe430e73134e43f675d299625bd
YouSay <yousay.sd@gmail.com>
2022-08-17 10:38:42 -0300

    add link to logo

--- components/organisms/Header/Header.jsx
+++ components/organisms/Header/Header.jsx
@@ -1,6 +1,7 @@
 import Container from 'components/atoms/Container/Container';
 import ImageWp from 'components/atoms/ImageWp/ImageWp';
 import Navbar from 'components/molecules/Navbar/Navbar';
+import Link from 'next/link';
 import styles from './Header.module.scss'
 
 const Header = ({ acf: {header} }) => {
@@ -12,13 +13,17 @@ const Header = ({ acf: {header} }) => {
       >
         <div className={styles.logo}>
           {header?.image &&
-            <ImageWp
-              image={header.image}
-              isFallback={false}
-              width='100%'
-              height='30%'
-              objectFit='contain'
-            />
+            <Link href='/'>
+              <a>
+                <ImageWp
+                  image={header.image}
+                  isFallback={false}
+                  width='100%'
+                  height='30%'
+                  objectFit='contain'
+                />
+              </a>
+            </Link>
           }
         </div>
         <Navbar />
